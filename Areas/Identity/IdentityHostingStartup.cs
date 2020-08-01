6ec911fac8a447bdad689724ff0cc96840b6fae3
Shawn C <scooter4356@gmail.com>
2020-08-01 14:01:35 -0500

    identity scaffoloded, but not showing up on bar

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Roulette_Identity.Areas.Identity.Data;
+
+[assembly: HostingStartup(typeof(Roulette_Identity.Areas.Identity.IdentityHostingStartup))]
+namespace Roulette_Identity.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<RouletteDbContext>(options =>
+                    options.UseMySql(
+                        context.Configuration.GetConnectionString("DefaultConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<RouletteDbContext>();
+            });
+        }
+    }
+}
